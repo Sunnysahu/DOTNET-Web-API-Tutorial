4382b9e8b1bc7414580d19c1b47ffc9a22a47ae6
Sunny Sahu <sunnysahu8210@gmail.com>
2025-04-20 19:44:14 +0530

    Added Domain Models

--- NZWalks/NZWalksAPI/README.md
+++ NZWalks/NZWalksAPI/README.md
@@ -1,1 +1,8 @@
 ﻿# Hello Guys, This is a Web API Project
+
+<img src="./Assets/NZWalks DDD.jpg" alt="My Image" />
+
+# Create Domain Models Based on Above Image
+
+- ### Create Model Folder --> Domain Folder --> Difficulty.cs, Walk.cs & Region.cs class 
+- ### Write the Your Code and then build a releationship b/w these Domans Model
--- NZWalks/NZWalksAPI/obj/Debug/net8.0/NZWalksAPI.AssemblyInfo.cs
+++ NZWalks/NZWalksAPI/obj/Debug/net8.0/NZWalksAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb12ebdfcf8aa11c89cbb1667d4c555c77bd7c0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3514f73120b04cde6056995ed132323d14d533a")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
